2179c2e002f1fef199682ee773aa7433b93d3663
Bryan Miller <bmilcs@yahoo.com>
2022-10-18 18:35:38 -0400

    Style & add header to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./scss/index.scss"
+import { makeElement } from "./modules/utils/make-element";
+import { header } from "./modules/header.js";
 
-
-console.log("Welcome to bmilcs Todo List");
+document.querySelector("body").appendChild(header);
 
