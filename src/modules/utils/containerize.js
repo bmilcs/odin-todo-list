e76bd64693297b04ab224401a9395d33823085be
Bryan Miller <bmilcs@yahoo.com>
2022-10-18 18:20:21 -0400

    Add containerize module

---
+++ src/modules/utils/containerize.js
@@ -0,0 +1,16 @@
+import { makeElement } from './make-element';
+
+// @returns a new div with a class of divClassName and
+//          appends all childElements elements to it
+export const containerize = (divClassName, ...childElements) => {
+  if ( !divClassName ) return;
+  
+  const container = makeElement("div", divClassName);
+  const children = Array.from(childElements);
+
+  console.log(children);
+
+  children.forEach(child => container.appendChild(child));
+
+  return container;
+}
