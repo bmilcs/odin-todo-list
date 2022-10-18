1f3de687e6b4970dee8a272534feda0c25b9f5d1
Bryan Miller <bmilcs@yahoo.com>
2022-10-18 18:34:52 -0400

    Add header.js

---
+++ src/modules/header.js
@@ -0,0 +1,14 @@
+import { makeElement } from "./utils/make-element";
+import { containerize } from "./utils/containerize";
+import headerIcon from "../assets/header-icon-2.svg";
+
+const createHeader = () => {
+  return containerize("header-container",
+    makeElement("h1", "title-h1", "TODO"),
+    makeElement("img", "header-icon", "Todo List Icon", "", headerIcon),
+  );
+}
+
+const header = createHeader();
+
+export { header };
