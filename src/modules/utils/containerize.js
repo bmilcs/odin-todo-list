fa54d75ae43b106c8d1456d97a5a472b3c6577d6
Bryan Miller <bmilcs@yahoo.com>
2022-10-21 16:15:00 -0400

    Add existing node functionality to containerize

--- src/modules/utils/containerize.js
+++ src/modules/utils/containerize.js
@@ -1,13 +1,27 @@
 import makeElement from './make-element';
 
-// @returns a new div with a class of divClassName and
-//          appends all childElements elements to it
-export default function containerize(divClassName, ...childElements) {
-  if ( !divClassName ) return;
-  
-  const container = makeElement("div", divClassName);
-  const children = Array.from(childElements);
+// @nodeOrClassesForNewDiv: accepts an existing node for the container or
+//                          or space-separated class names for a new div container
+// @childNodes: nodes that are nested inside the above container (appendChild)
+// @return container node with childNodes appended to it
+export default function containerize(nodeOrClassesForNewDiv, ...childNodes) {
+  // require at least 2 arguments
+  if ( !nodeOrClassesForNewDiv || !childNodes) return;
 
+  let container;
+
+  // if 1st argument is node element, use it for the container
+  if ( nodeOrClassesForNewDiv.nodeType ) 
+    container = nodeOrClassesForNewDiv;
+  else {
+    // create a new div & add classes to it, separated by space
+    container = makeElement("div", nodeOrClassesForNewDiv);
+    nodeOrClassesForNewDiv
+      .split(" ")
+      .forEach((cls) => container.classList.add(cls));
+  }
+
+  const children = Array.from(childNodes);
   children.forEach(child => container.appendChild(child));
 
   return container;
