37e320204dfd0c4b363e32f72ec006ae8b83947f
Bryan Miller <bmilcs@yahoo.com>
2022-10-22 11:37:57 -0400

    Fix sample data export method & naming

--- src/modules/sample-data.js
+++ src/modules/sample-data.js
@@ -1,9 +1,11 @@
-import Task from "./task";
 import TaskList from "./task-list";
 
-export const sampleList = () => {
+const prepSampleList = () => {
   const list = new TaskList("Default");
   list.addTask("Finish my todo list project", "11/2/2022");
   list.addTask("Complete Odin Project", "1/1/2023");
   return list;
 }
+
+const list = prepSampleList();
+export default list;
