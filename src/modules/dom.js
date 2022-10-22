96230e95d578e14fb11737d0fa456360f26cc517
Bryan Miller <bmilcs@yahoo.com>
2022-10-22 11:40:05 -0400

    Add basic sidebar, fix naming & refactor functions

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,9 +1,9 @@
 import makeElement from "./utils/make-element";
 import containerize from "./utils/containerize";
 import header from "./header.js";
-import main from "./main.js";
 import footer from "./footer.js";
-import { sampleList } from "./sample-data";
+import main from "./main.js";
+import sampleList from "./sample-data";
 import checkedImg from "../assets/checkbox-filled.svg";
 import uncheckedImg from "../assets/checkbox-empty.svg";
 
@@ -12,42 +12,59 @@ import uncheckedImg from "../assets/checkbox-empty.svg";
 //
 
 export const renderPage = () => {
+  renderMainLayout();
+  renderSidebar();
+  renderSampleData();
+};
+
+const renderMainLayout = () => {
   const body = document.querySelector("body");
-  const elements = [
-    header,
-    main,
-    footer
-  ];
-  elements.forEach(element => body.appendChild(element));
+  const elements = [header, main.parent, footer];
+  elements.forEach((element) => body.appendChild(element));
+};
+
+const renderSidebar = () => {
+  const title = makeElement("h2", "sidebar-title", "Sidebar Title");
+  const p = makeElement("p", "sidebar-p", "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.");
+  main.sidebar.appendChild(title);
+  main.sidebar.appendChild(p);
 }
 
 //
 // .renderList(): renders a todo list
 //
 
-export const renderList = (listObj) => {
+export const prepListElement = (listObj) => {
   const container = makeElement("div", "list-container");
   const title = makeElement("h2", "list-title", listObj.getName());
-  const tasks = renderAllTasks(listObj.getTasks());
+  const tasks = prepAllTaskElements(listObj.getTasks());
   container.appendChild(title);
-  tasks.forEach((node) => container.appendChild(node));
+  tasks.forEach((element) => container.appendChild(element));
   return container;
-}
+};
 
-const renderAllTasks = (taskArray) => {
-  return taskArray.map(task => renderTask(task));
-}
+const prepAllTaskElements = (taskArray) => {
+  return taskArray.map((task) => prepTaskElement(task));
+};
 
-const renderTask = (task) => {
+const prepTaskElement = (task) => {
   // const container = makeElement("div", "task-container");
   // const checkbox = makeElement("img", "task-checkbox unchecked", "", "", uncheckedImg);
-  // const descriptionNode = makeElement("p", "task-description", task.description)
+  // const descriptionElement = makeElement("p", "task-description", task.description)
 
-  return containerize("task-container", 
+  return containerize(
+    "task-container",
     makeElement("img", "task-status unchecked", "", "", uncheckedImg),
     makeElement("p", "task-description", task.getDescription()),
-    makeElement("p", "task-date", task.getDate()));
-}
+    makeElement("p", "task-date", task.getDate())
+  );
+};
 
+//
+// .renderSampleData
+//
 
-document.querySelector("body").appendChild(renderList(sampleList()));
+const renderSampleData = () => {
+  const list = prepListElement(sampleList);
+  main.content.appendChild(list);
+};
