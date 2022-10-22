de595834a0e26990a313bcd436ce2fb9faf08c67
Bryan Miller <bmilcs@yahoo.com>
2022-10-22 13:19:21 -0400

    Add clickevent to checkbox & cleanup code
    Signed-off-by: Bryan Miller <bmilcs@yahoo.com>

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -7,17 +7,13 @@ import sampleList from "./sample-data";
 import checkedImg from "../assets/checkbox-filled.svg";
 import uncheckedImg from "../assets/checkbox-empty.svg";
 
-//
-// .renderPage(): render body of the page
-//
-
 export const renderPage = () => {
-  renderMainLayout();
+  renderLayout();
   renderSidebar();
-  renderSampleData();
+  renderMainContent();
 };
 
-const renderMainLayout = () => {
+const renderLayout = () => {
   const body = document.querySelector("body");
   const elements = [header, main.parent, footer];
   elements.forEach((element) => body.appendChild(element));
@@ -25,16 +21,16 @@ const renderMainLayout = () => {
 
 const renderSidebar = () => {
   const title = makeElement("h2", "sidebar-title", "Sidebar Title");
-  const p = makeElement("p", "sidebar-p", "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.");
+  const p = makeElement(
+    "p",
+    "sidebar-p",
+    "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+  );
   main.sidebar.appendChild(title);
   main.sidebar.appendChild(p);
-}
+};
 
-//
-// .renderList(): renders a todo list
-//
-
-export const prepListElement = (listObj) => {
+const prepListElement = (listObj) => {
   const container = makeElement("div", "list-container");
   const title = makeElement("h2", "list-title", listObj.getName());
   const tasks = prepAllTaskElements(listObj.getTasks());
@@ -48,23 +44,32 @@ const prepAllTaskElements = (taskArray) => {
 };
 
 const prepTaskElement = (task) => {
-  // const container = makeElement("div", "task-container");
-  // const checkbox = makeElement("img", "task-checkbox unchecked", "", "", uncheckedImg);
-  // const descriptionElement = makeElement("p", "task-description", task.description)
-
-  return containerize(
-    "task-container",
-    makeElement("img", "task-status unchecked", "", "", uncheckedImg),
-    makeElement("p", "task-description", task.getDescription()),
-    makeElement("p", "task-date", task.getDate())
+  const container = makeElement("div", "task-container");
+  const checkbox = makeElement(
+    "img",
+    "task-status unchecked",
+    "",
+    "",
+    uncheckedImg
   );
+  const description = makeElement(
+    "p",
+    "task-description",
+    task.getDescription()
+  );
+  const dueDate = makeElement("p", "task-date", task.getDate());
+
+  checkbox.addEventListener("click", toggleTaskStatus);
+
+  return containerize(container, checkbox, description, dueDate);
 };
 
-//
-// .renderSampleData
-//
+const toggleTaskStatus = (e) => {
+  const element = e.target;
+  alert(`toggle status clicked on: ${element.classList}`);
+};
 
-const renderSampleData = () => {
-  const list = prepListElement(sampleList);
-  main.content.appendChild(list);
+const renderMainContent = () => {
+  const defaultList = prepListElement(sampleList);
+  main.content.appendChild(defaultList);
 };
