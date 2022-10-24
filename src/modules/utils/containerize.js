b6ca0fda6f5ba848d498191e6113e8bc79a746c3
Bryan Miller <bmilcs@yahoo.com>
2022-10-24 18:46:17 -0400

    Refactor containerize to accept arrays of nodes

--- src/modules/utils/containerize.js
+++ src/modules/utils/containerize.js
@@ -1,18 +1,18 @@
-import makeElement from './make-element';
+import makeElement from "./make-element";
 
 // @nodeOrClassesForNewDiv: accepts an existing node for the container or
 //                          or space-separated class names for a new div container
-// @childNodes: nodes that are nested inside the above container (appendChild)
+// @childNodes: individual nodes Or arrays of nodes that will be nested
+//              inside the above container(appendChild)
 // @return container node with childNodes appended to it
 export default function containerize(nodeOrClassesForNewDiv, ...childNodes) {
   // require at least 2 arguments
-  if ( !nodeOrClassesForNewDiv || !childNodes) return;
+  if (!nodeOrClassesForNewDiv || !childNodes) return;
 
   let container;
 
   // if 1st argument is node element, use it for the container
-  if ( nodeOrClassesForNewDiv.nodeType ) 
-    container = nodeOrClassesForNewDiv;
+  if (nodeOrClassesForNewDiv.nodeType) container = nodeOrClassesForNewDiv;
   else {
     // create a new div & add classes to it, separated by space
     container = makeElement("div", nodeOrClassesForNewDiv);
@@ -21,8 +21,19 @@ export default function containerize(nodeOrClassesForNewDiv, ...childNodes) {
       .forEach((cls) => container.classList.add(cls));
   }
 
-  const children = Array.from(childNodes);
-  children.forEach(child => container.appendChild(child));
+  const addToContainer = (x) => {
+    x.nodeType
+      ? container.appendChild(x)
+      : console.warn(`${x} is not an element!`);
+  };
 
+  // loop through all arguments: if node, append it to container.
+  // if array, loop & append all to container
+  const children = Array.from(childNodes);
+  children.forEach((child) => {
+    child.constructor === Array
+      ? child.forEach((childOfChild) => addToContainer(childOfChild))
+      : addToContainer(child);
+  });
   return container;
 }
